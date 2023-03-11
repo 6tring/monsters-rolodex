9ef53e0e198584ce63f138e09f7fbd33bc626a27
pjames74 <peter@peterciluzzi.com>
2023-03-11 00:03:10 -0500

    begin rewriting class components as functional components

--- src/App.js
+++ src/App.js
@@ -1,59 +1,84 @@
-import { Component } from 'react';
+import { useState, useEffect } from "react";
 import CardList from "./components/card-list/card-list.component";
 import SearchBox from "./components/search-box/search-box.component";
 import './App.css';
 
-class App extends Component {
-  constructor() {
-    super();
-    this.state = {
-      monsters: [],
-      searchField: ""
-    };
-    console.log("constructor");
+const App = () => {
+
+  const [searchField, setSearchField] = useState("");
+  const [monsters, setMonsters] = useState([]);
+  console.log(searchField);
+
+  const onSearchChange = (event) => {
+    const searchFieldString = event.target.value.toLocaleLowerCase();
+    setSearchField(searchFieldString);
   }
 
-  componentDidMount() {
-    console.log("componentDidMount");
-    fetch("Https://jsonplaceholder.typicode.com/users")
-      .then((response) => response.json())
-      .then((users) => {
-        this.setState(
-          () => {
-            return { monsters: users }
-          },
-          () => {
-            console.log(this.state);
-          }
-        )
-      })
-  }
+  const filteredMonsters = monsters.filter((monster) => {
+    return monster.name.toLocaleLowerCase().includes(searchField)
+  });
 
-  onSearchChange = (event) => {
-    console.log(event.target.value);
-    const searchField = event.target.value.toLocaleLowerCase();
-    this.setState(() => {
-      return { searchField };
-    });
-    console.log("onSearchChange");
-  }
 
-  render() {
-    console.log("render");
-    const { monsters, searchField } = this.state;
-    const { onSearchChange } = this;
-    const filteredMonsters = monsters.filter((monster) => {
-      return monster.name.toLocaleLowerCase().includes(searchField)
-    });
-
-    return (
-      <div className="App">
-        <h1 className="app-title">Monsters Redux</h1>
-        <SearchBox onChangeHandler={onSearchChange} placeholder= "Search monsters" className="monsters-search-box"/>
-        <CardList monsters={filteredMonsters}/>
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <h1 className="app-title">Monsters Redux</h1>
+      <SearchBox onChangeHandler={onSearchChange} placeholder= "Search monsters" className="monsters-search-box"/>
+      <CardList monsters={filteredMonsters}/>
+    </div>
+  )
 }
+
+// class App extends Component {
+//   constructor() {
+//     super();
+//     this.state = {
+//       monsters: [],
+//       searchField: ""
+//     };
+//     console.log("constructor");
+//   }
+//
+//   componentDidMount() {
+//     console.log("componentDidMount");
+//     fetch("Https://jsonplaceholder.typicode.com/users")
+//       .then((response) => response.json())
+//       .then((users) => {
+//         this.setState(
+//           () => {
+//             return { monsters: users }
+//           },
+//           () => {
+//             console.log(this.state);
+//           }
+//         )
+//       })
+//   }
+//
+//   onSearchChange = (event) => {
+//     console.log(event.target.value);
+//     const searchField = event.target.value.toLocaleLowerCase();
+//     this.setState(() => {
+//       return { searchField };
+//     });
+//     console.log("onSearchChange");
+//   }
+//
+//   render() {
+//     console.log("render");
+//     const { monsters, searchField } = this.state;
+//     const { onSearchChange } = this;
+//     const filteredMonsters = monsters.filter((monster) => {
+//       return monster.name.toLocaleLowerCase().includes(searchField)
+//     });
+//
+//     return (
+//       <div className="App">
+//         <h1 className="app-title">Monsters Redux</h1>
+//         <SearchBox onChangeHandler={onSearchChange} placeholder= "Search monsters" className="monsters-search-box"/>
+//         <CardList monsters={filteredMonsters}/>
+//       </div>
+//     );
+//   }
+// }
 
 export default App;
